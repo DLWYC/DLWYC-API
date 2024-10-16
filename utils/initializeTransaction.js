a540da48679daf0a10f4a99bb9228753cfaaca8a
Timmy-Tee <timmyaof02@gmail.com>
2024-10-16 17:28:20 +0100

    Done - Prone To Changes

--- utils/initializeTransaction.js
+++ utils/initializeTransaction.js
@@ -1,30 +1,5 @@
 const paystack = require("paystack")(process.env.PAYSTACK_SECRET_KEY);
 
-//   paymentData = {
-//     email: email,
-//     reference: new Date().getTime().toString(),
-//     callback_url: "http://localhost:5174/payment/successful",
-//   };
-
-//   if (paymentOption === "Multiple") {
-//     payStackData = {
-//       ...paymentData,
-//       amount: (5000 * 100) * noOfCampersToPayFor,
-//       metadata: {
-//         custom_fields: [
-//           {
-//             display_name: "Name Of Campers Paid For",
-//             variable_name: noOfUnpaidCampersOption,
-//           },
-//         ],
-//       },
-//     };
-//   }
-//   else{
-//      payStackData = {...paymentData, amount: 500 * 100};
-//   }
-
-//   console.log("Payment" + payStackData.amount)
 
 const initializeTransaction = async (payStackData) =>{
    return await paystack.transaction
